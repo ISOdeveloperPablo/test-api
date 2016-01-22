39ffcb5b39350479ff9388fe374a8a4c7d17e131
isodeveloper2 <jacob_latter@isodevelopers.com>
2016-01-22 11:05:21 -0500

    fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ canada = PoBox.create!(box_number: 4325435, city: "Bobtown", country: "Canada", 
 kenya = PoBox.create!(box_number: 7568758, city: "Nairobi", country: "Kenya")
 s_africa = PoBox.create!(box_number: 5869456, city: "Johannesburg", country: "South Africa")
 
-User.create(email: "admin@hotmail.com", password: "password", first_name: "Test",  last_name: "User", phone_number: ENV["TEST_PHONE_NUMBER"], role: "Admin" , po_box_id: 99, country_code: ENV["TEST_COUNTRY_CODE"])
-User.create(email: "employee@hotmail.com", password: "password", first_name: "employee",  last_name: "employee", phone_number: ENV["TEST_PHONE_NUMBER"], role: "Employee" , po_box_id: 99, country_code: ENV["TEST_COUNTRY_CODE"])
-User.create(email: "recipient@hotmail.com", password: "password", first_name: "Test",  last_name: "User", phone_number: ENV["TEST_PHONE_NUMBER"], po_box_id: 99, country_code: ENV["TEST_COUNTRY_CODE"])
+User.create(email: "admin@hotmail.com", password: "password", first_name: "Test",  last_name: "User", phone_number: 123456789, role: "Admin" , po_box_id: 99, country_code: 1)
+User.create(email: "employee@hotmail.com", password: "password", first_name: "employee",  last_name: "employee", phone_number: 123456789, role: "Employee" , po_box_id: 99, country_code: 1)
+User.create(email: "recipient@hotmail.com", password: "password", first_name: "Test",  last_name: "User", phone_number: 123456789, po_box_id: 99, country_code: 1)
 
 3.times {
-  User.create!(email: Faker::Internet.email, password: Faker::Internet.password(8), po_box: canada, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: ENV["TEST_PHONE_NUMBER"] , country_code: ENV["TEST_COUNTRY_CODE"])
-  User.create!(email: Faker::Internet.email, password: Faker::Internet.password(8), po_box: kenya, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: ENV["TEST_PHONE_NUMBER"] , country_code: ENV["TEST_COUNTRY_CODE"])
-  User.create!(email: Faker::Internet.email, password: Faker::Internet.password(8), po_box: s_africa, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: ENV["TEST_PHONE_NUMBER"] , country_code: ENV["TEST_COUNTRY_CODE"])
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password(8), po_box: canada, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: 123456789 , country_code: 1)
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password(8), po_box: kenya, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: 123456789 , country_code: 1)
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password(8), po_box: s_africa, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: 123456789 , country_code: 1)
 }
