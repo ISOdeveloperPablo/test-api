e212b737f5f6f0fcca2a238449462fe770ce67cb
isodeveloper2 <jacob_latter@isodevelopers.com>
2016-01-22 11:24:12 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
 
   ### API ###
-  namespace :api, :path => "", :constraints => {:subdomain => "api"}, :defaults => {:format => :json} do
+  namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :users
     end
