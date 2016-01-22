b077126e76f1d9c545b073bf8a4b0db283c95d63
isodeveloper2 <jacob_latter@isodevelopers.com>
2016-01-22 13:33:15 -0500

    jsonp change back

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -3,7 +3,7 @@ class API::V1::UsersController < ApplicationController
   def index
     @users = User.all
     respond_to do |format|
-      format.jsonp { render :jsonp => @users }
+      format.json { render :json => @users }
     end
   end
 end
